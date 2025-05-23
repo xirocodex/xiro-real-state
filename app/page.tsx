e51d1c64a9284df70fc0855eda88260719e762c5
mazed <ablmazed@gmail.com>
2025-05-23 11:58:06 +0600

    update 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import { Camera } from 'lucide-react'
+
 export default function page() {
   return (
     <div>
       <div className="flex bg-gray-400">
         <div>Xiro-Real-State</div>
-        <div>Login</div>
+        <div>
+          {' '}
+          <Camera />{' '}
+        </div>
       </div>
 
       <div className="min-h-screen bg-blue-50">
